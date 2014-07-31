7e0bf649aa70306819a918dc9270c00f2b6870ab
Mark A. Folashade <ayodeji.folashade@gmail.com>
2014-07-31 09:53:42 -0400

    Changed Salesdata

--- SalesTool/src/ca/demo/salestool/SalesData.java
+++ SalesTool/src/ca/demo/salestool/SalesData.java
@@ -1,9 +1,11 @@
 package ca.demo.salestool;
 
 public class SalesData {
-	int data[] = {0, 4, 42};
+	int data[] = {0, 4, 42, 99};
 	
 	public void display(){
-		System.out.println("TODO: Implement data display");
+		for (int i: data){
+			System.out.println(i);
+		}
 	}
 }
